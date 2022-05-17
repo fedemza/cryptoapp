7602def7ad01d2ad0332d2e43e5c587ead844f8d
Federico Voloschin <fedevoloschin@gmail.com>
2022-05-17 13:25:24 -0300

    header y nav

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,66 @@
 import React from "react";
+import logo from "../../assets/logo.png";
+import { AppBar, Toolbar, Typography, Container } from "@mui/material";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <AppBar position="static">
+        <Container
+          maxWidth="xl"
+          sx={{
+            flexGrow: 1,
+            display: { md: "flex" },
+            justifyContent: "center",
+          }}
+        >
+          <Toolbar>
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              sx={{
+                display: { md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              <img
+                src={logo}
+                height="40px"
+                width="40px"
+                alt=""
+                padding="1px"
+                marginLeft="80px"
+                sx={{ ml: 10 }}
+              />
+            </Typography>
+
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              sx={{
+                mr: 2,
+                ml: 2,
+                display: { md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              CRIPTODEMIA
+            </Typography>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/navbar/Nav.js
+++ src/components/navbar/Nav.js
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  Menu,
+  Container,
+  Button,
+  MenuItem,
+} from "@mui/material";
+import { Menu as MenuIcon, Adb as AdbIcon } from "@mui/icons-material";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const [anchorElNav, setAnchorElNav] = useState(null);
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { md: "flex" },
+              justifyContent: "center",
+            }}
+          >
+            <Link to="/" style={{ textDecoration: "none" }}>
+              <Button
+                size="large"
+                sx={{
+                  my: 2,
+                  color: "white",
+                  display: "block",
+                }}
+              >
+                HOME
+              </Button>
+            </Link>
+            <Link to="/coins" style={{ textDecoration: "none" }}>
+              <Button
+                size="large"
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                COINS
+              </Button>
+            </Link>
+            <Link to="/about" style={{ textDecoration: "none" }}>
+              <Button
+                size="large"
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                ABOUT
+              </Button>
+            </Link>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
 
 export default Nav;
